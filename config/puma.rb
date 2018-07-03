669ca6b7f78efc4377e9e1f83aac6adf2a405683
Lyubchenko Yuriy <undeadspez@gmail.com>
2018-07-03 21:53:07 +0300

    Final setup

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,6 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# To deploy to Heroku uncomment the following line
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -11,5 +13,7 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
 end
